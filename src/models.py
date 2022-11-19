7d026d307f289d5428b15dd426c300c751db5d46
MireyaCR <mireya.mcr@gmail.com>
2022-11-19 11:21:07 +0000

    datamodelinginstagram

--- src/models.py
+++ src/models.py
@@ -10,43 +10,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    user_name = Column(String(250),primary_key=True)
-    email = Column(String(250))
-    password= Column(String(250))
-    
-
 class Followers(Base):
     __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
     followers_id = Column(Integer, ForeignKey('user.id'))
     user_id = Column(Integer, ForeignKey('user.id'))
-    status = Column(String(250))
-    followers = relationship(User)
+  
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250))
+    password= Column(String(250))
+    User = relationship(Followers)
 
-class Media(enum.Enum):
-    photo = 'photo'
-    pictures = 'pictures'
-    video = 'video'
-    
+
+
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    photo = Column(String(250))
     description = Column(String(250))
     created = Column(String(250))
     updated = Column(String(250))
-    types= Column(Enum(Media))
     followers = relationship(User)
+
+class Tipos(enum.Enum):
+    photo = 'photo'
+    pictures = 'pictures'
+    video = 'video'
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    types= Column(Enum(Tipos))
+    url= Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
 class Comments(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
     content = Column(String(250))
     created= Column(String(250))
